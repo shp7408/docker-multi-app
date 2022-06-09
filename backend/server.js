9914e1aa5b7e5abb4a348417a97bd8ceb929f9e6
shp7408 <62336151+shp7408@users.noreply.github.com>
2022-06-09 12:15:47 +0900

    주석 설명 수정

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
+/* 벡엔드의, 시작점이 되는 파일 */
+
+// 필요한 모듈 가져오기
 const express = require("express");
 
+// export한 poo을 server.js에서 불러옵니다. db.js 파일 안에 pool이 들어 있기 때문에,
+// db.js 파일을 가져오면 pool을 가져오게 된다.
 const db = require('./db');
 
+// 익스프레스 서버 생성
 const app = express();
 
+// JSON 형태로 전달되는 요청의 본문을 해석할 수 있게 등록
 app.use(express.json());
 
 app.listen(5000, () => {
@@ -23,7 +30,7 @@ app.get('/api/values', function (req, res) {
     })
 })
 
-// 클라이언트에서 입력한 값을 DB이 lists 테이블에 넣어주기
+// 클라이언트에서 입력한 값을 DB의 lists 테이블에 넣어주기
 app.post('/api/value', function (req, res, next) {
     // 데이터베이스에 값 넣어주기
     db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`,
